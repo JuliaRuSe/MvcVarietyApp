939b7f4810ce8a32de667cb8ccae105b8ea1e301
rudin <rudin@LAPTOP-6QEMVORU>
2026-05-08 01:57:26 +0300

    Add project files.

---
+++ MvcCreditApp/Models/Credit.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace MvcCreditApp.Models
+{
+    public class Credit
+    {
+        // ID кредита
+        public virtual int CreditId { get; set; }
+        // Название
+        [DisplayName("Название")]
+        public virtual string Head { get; set; }
+        // Период, на который выдается кредит
+        [DisplayName("Период")]
+        public virtual int Period { get; set; }
+        // Максимальная сумма кредита
+        [DisplayName("Максимальная сумма")]
+        public virtual int Sum { get; set; }
+        // Процентная ставка
+        [DisplayName("Процентная ставка")]
+        public virtual int Procent { get; set; }
+    }
+}
